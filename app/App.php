a8927adbb1320b80a865850a4e1e4ea3fe546eae
phendan <10546560+phendan@users.noreply.github.com>
2023-01-30 17:17:00 +0100

    Change autoloader to composer

--- app/App.php
+++ app/App.php
@@ -7,7 +7,7 @@ use App\Router;
 class App {
     public function __construct()
     {
-        $this->autoloadClasses();
+        // $this->autoloadClasses();
 
         $router = new Router;
 
@@ -18,20 +18,19 @@ class App {
         $controller = new $requestedController;
 
         $request = new Request($params);
-
         $controller->{$requestedMethod}($request);
     }
 
-    private function autoloadClasses()
-    {
-        spl_autoload_register(function ($namespace) {
-            $projectNamespace = 'App\\';
-            $className = str_replace($projectNamespace, '', $namespace);
-            $filePath = __DIR__ . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
+    // private function autoloadClasses()
+    // {
+    //     spl_autoload_register(function ($namespace) {
+    //         $projectNamespace = 'App\\';
+    //         $className = str_replace($projectNamespace, '', $namespace);
+    //         $filePath = __DIR__ . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
 
-            if (file_exists($filePath)) {
-                require_once $filePath;
-            }
-        });
-    }
+    //         if (file_exists($filePath)) {
+    //             require_once $filePath;
+    //         }
+    //     });
+    // }
 }
--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-require_once '../app/helpers.php';
-require_once '../app/App.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../app/helpers.php';
 
 $app = new App\App;
